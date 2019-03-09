ad91a7e5d6cfc69d012ab3221c8e6c8423cad552
watabee <watabee.dev@gmail.com>
2019-03-09 10:56:14 +0900

    Fix cocoapods-keys error because of no xcodeproj file

--- ios/scripts/setup.sh
+++ ios/scripts/setup.sh
@@ -3,6 +3,6 @@
 RAKUTEN_APP_ID=$1
 
 bundle install
-bundle exec pod keys set "RakutenAppId" "$RAKUTEN_APP_ID"
 xcodegen generate
+bundle exec pod keys set "RakutenAppId" "$RAKUTEN_APP_ID"
 bundle exec pod install --repo-update
